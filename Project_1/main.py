fc3e4f6d2acedbc7000d13d904f4b99a5326c91e
Alexandre <alexurluescu23@gmail.com>
2022-10-14 13:31:03 +0300

    Inceputul

---
+++ Project_1/main.py
@@ -0,0 +1,10 @@
+def main():
+    """Main function of the prject
+    """
+
+    print('hello world')
+    student_name = get_student_name()
+    student_age = get_student_age()
+    student_year_of_study = get_yaer
+
+main()
