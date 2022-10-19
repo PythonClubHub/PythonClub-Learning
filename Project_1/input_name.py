fa566ba8b90ead26ad9ad55f28f8c41c20d1b46d
Alexandre <alexurluescu23@gmail.com>
2022-10-19 18:23:49 +0300

    Project1:input_name:version 3.0

--- Project_1/input_name.py
+++ Project_1/input_name.py
@@ -1,5 +1,6 @@
 # File to be modified by Alex
 
+
 def get_student_name():
     """Function that asks the user to type the name of a student and return the name with first letter in each name as Upper case.
     The function should return a valid name that contains only charactes and has a maximun of 30 characters.
@@ -9,10 +10,26 @@ def get_student_name():
     name = str(input("What's your name?: "))
     lastName = str(input("What's your last name?: "))
 
-    print(name.capitalize()+ " " + lastName.capitalize())
+    name_user = name[:15]
+    lastName_user = lastName[:15]
+
+    validation_name = name_user.isalpha()
+    validation_lastName = lastName_user.isalpha()
+
+    if validation_name == False:
+        print("Ati tastat un nume gresit, va rog sa folositi doar litere")
+        name = str(input("What's your name?: "))
+        name_user = name[:15]
+    
+    if validation_lastName == False:
+        print("Ati tastat un prenume gresit, va rog sa folositi doar litere")
+        lastName = str(input("What's your last name?: "))
+        lastName_user = lastName[:15]
 
     # return "John Doe"
 
+    print(name_user.capitalize() + " " + lastName_user.capitalize())
     return name.capitalize() + " " + lastName.capitalize()
+
 
 get_student_name()
