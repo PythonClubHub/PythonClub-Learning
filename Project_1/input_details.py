952739d15a3325e9a96a14002ef540d67de48b09
Cîrstea Andrei-Călin <andrei.cirstea98@gmail.com>
2022-10-30 21:23:29 +0200

    Wrote function for student_year_of_study

--- Project_1/input_details.py
+++ Project_1/input_details.py
@@ -1,12 +1,32 @@
 #File to be modified by Andrei
 
+from Project_1.util import DEBUG
+import main
+import util
+import math
+
 def get_year_of_study():
-    """Function that asks the user to type the year of study of a student and return the age as numerical value.
+    """Function that asks the user to type the year of study of a student and return the year of study as numerical value.
     The function should return a valid study year that contains only numbers and has a maximun value of 4.
     Argument: None
     Return: a numerical value
     """
-    return 3
+    
+    while True:
+        try:
+            student_year_of_study = int(input("What year of study are you in?(only numbers 1-4)"))
+            break
+        except:
+            print("You either entered a number outside the range , 1-4, or a literal character")
+    
+    if student_year_of_study in range(1,5):
+        if DEBUG:
+            print(student_year_of_study)
+        return student_year_of_study
+    else:
+        print("You either entered a number outside the range , 1-4, or a literal character")
+        get_year_of_study()
+        
 
 
 def get_field_of_study():
