7e47d48d7ad8d29c625a6207e9a196b4b89e6681
Stefanescu Andreea <andreeast2002@gmail.com>
2022-10-30 21:46:59 +0200

    wrote the function get_student_age

--- Project_1/input_age.py
+++ Project_1/input_age.py
@@ -1,9 +1,28 @@
 # File to be modified by Andreea
 
+from Project_1.util import DEBUG
+import main
+import util
+
 def get_student_age():
     """Function that asks the user to type the age of a student and return the age as numerical value.
-    The function should return a valid age that contains only numbers and has a maximun value of 99.
+    The function should return a valid age that contains only numbers and has a maximum value of 99.
     Argument: None
     Return: a numerical value
     """
-    return 21
+    
+    while True:
+        try :
+            student_age = int(input("Insert your age please (must be a numerical value between 0 and 99) : "))
+            break
+        except:
+            print("You must enter a valid number ( 0-99)")
+    
+    if student_age in range(1,98):
+        if DEBUG:
+            print(student_age)
+        return student_age
+    else:
+        print("You must enter a valid number ( 0-99)")
+        get_student_age()
+
