9b24546fdcc2037aeac24f8c0d7454585afd8f40
Timotei Sandru <timotei.sandru2022@gmail.com>
2023-10-29 20:23:11 +0200

    Add commits file
    May look like git --help but here you will find
    the essentials commands.

---
+++ Git workflow/commands.py
@@ -0,0 +1,10 @@
+# git clone                 - Clone repository
+# git log                   - Show log commits
+# git pull                  - Fetches from repository
+# git push                  - Upload commits to remote repository
+# git add                   - Stage files
+# git commit                - Save staged files
+# git switch                - Change branch
+# git checkout              - General operation (see git checkout --help)
+# git status                - Show working tree status 
+# git merge                 - Join two branches
