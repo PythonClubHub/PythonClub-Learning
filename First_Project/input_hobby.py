d5b3d5f888fe6236ff9f6d287bc4b0fe671cd5a5
Alex <alexmihaita03@gmail.com>
2023-11-11 16:43:51 +0200

    Add funvtionallity to Student Hobby and solved the issues.

--- First_Project/input_hobby.py
+++ First_Project/input_hobby.py
@@ -1,5 +1,7 @@
 ''' File to be modified by Mihai '''
 
+import re
+
 def get_student_hobby():
     '''
     Function that asks the user to type their hobby and return the hobby as string value.
@@ -10,18 +12,24 @@ def get_student_hobby():
     Valid_Hobby=False
 
     while Valid_Hobby is False:
-        Hobby=input("What are your hobbies? (input only characters) ")
-        is_string=Hobby.isalpha()
+        Hobby=input("What is your hobby? (input only characters) ")
+        Hobby=Hobby.lower()
         size_of_string = len(Hobby)
+        has_symbols=re.findall("[!@#$%^&*()_+=]",Hobby)
+        has_numbers=re.search("[0-9]",Hobby)
+        has_space_at_start=re.match(" ",Hobby)
+      
 
-        if (is_string is True) and size_of_string > 0 and size_of_string <= 20 :
-            Valid_Hobby = True
-            return Hobby
-        
+        if has_numbers or has_symbols or has_space_at_start:
+            print ("You either entered a symbol,a number or only a space. Please try again!")
+
         elif size_of_string > 20 or size_of_string == 0 :
             print("You either entered more than 20 characters or entered nothing. Please meet these conditions! ")
+              
+        else:Valid_Hobby = True
+        
+    return Hobby
 
-        else: print ("Please input only characters!")
-
-get_student_hobby()
+if __name__ == "__main__":
+    get_student_hobby()
  
