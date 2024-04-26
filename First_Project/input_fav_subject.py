34e853d221e335e2e04dc0cc24ec18a0dc6c7f3f
AUTO\uie99388 <timotei.sandru@continental-corporation.com>
2024-04-26 12:05:48 +0300

    Nwebie part 4

---
+++ First_Project/input_fav_subject.py
@@ -0,0 +1,10 @@
+''' File to be modified by Newbie 4 '''
+
+def get_student_favourite_subject():
+    '''
+    Function that asks the user to type their favourite subject and return it as string value.
+    The function should return a valid string that contains only charactes and has a maximun of 20 characters.
+    Argument: None
+    Return: String
+    '''
+    return "Newbie 4 part"
