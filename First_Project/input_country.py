3264a074d308415bf15ca6d43303d5245ff2b320
AUTO\uie99388 <timotei.sandru@continental-corporation.com>
2024-05-31 14:40:24 +0300

    add newbie 5 task

---
+++ First_Project/input_country.py
@@ -0,0 +1,10 @@
+''' File to be modified by Newbie 5 '''
+
+def get_student_country():
+    '''
+    Function that asks the user which country they are from.
+    The function should return a valid string that contains only letters and has a maximum length of 20 characters.
+    Argument: None
+    Return: String
+    '''
+    return "Newbie 5 part"
--- First_Project/input_fav_subject.py
+++ First_Project/input_fav_subject.py
@@ -3,7 +3,7 @@
 def get_student_favourite_subject():
     '''
     Function that asks the user to type their favourite subject and return it as string value.
-    The function should return a valid string that contains only charactes and has a maximun of 20 characters.
+    The function should return a valid string that contains only charactes and has a maximum of 20 characters.
     Argument: None
     Return: String
     '''
